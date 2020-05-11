84923c2b28f1ddf6aeddf0e3499ff1284fb99f31
ilan07m <ilan07m@gmail.com>
2020-05-11 22:59:30 +0300

    feat: create new mongo db via api

--- src/databeses/mongo.py
+++ src/databeses/mongo.py
@@ -1,10 +1,8 @@
 import pymongo
 from ..ssh_commands import *
 from resources.login_details import *
-MONGO_BACKUP_ALL_COMMAND = "mongodump -u " + MONGO_ROOT_USER + " -p " + MONGO_ROOT_PASSWORD\
-                           + " --authenticationDatabase admin --out "  # Need to insert the backup file name
-MONGO_RESTORE_ALL_COMMAND = "mongorestore -u " + MONGO_ROOT_USER + " -p " + MONGO_ROOT_PASSWORD\
-                            + " --authenticationDatabase admin --drop "  # Need to insert the dmp file name
+MONGO_BACKUP_ALL_COMMAND = "mongodump -u {} -p {} --authenticationDatabase admin --out {}"  # Insert username, password, and the backup file path
+MONGO_RESTORE_ALL_COMMAND = "mongorestore -u {} -p {} --authenticationDatabase admin --drop {}"  # Insert username, password, and the dmp file path
 # TODO: Change dir where backup files are!
 MONGO_DMP_DIR = "~/mongobackups/{}"  # format with whole/db/collection
 LIST_COMMAND = 'cd {}; ls -A1'
@@ -12,21 +10,25 @@ RESOURCE_ERROR = 'Not a valid resourceType...'
 
 
 def mongo_dbs():
-    client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
-    dbsList = client.list_database_names()
-    return dbsList
+    try:
+        client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+        dbsList = client.list_database_names()
+        return dbsList
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to server: %s" % e)
+
 
 
 # TODO: Change the server name, the username and the password!!! + get backup file as parameter!!!
 def mongo_backup_all():
-    output = run_command(MONGO_BACKUP_ALL_COMMAND + "/path/to/backup/file_name",
+    output = run_command(MONGO_BACKUP_ALL_COMMAND.format(MONGO_ROOT_USER, MONGO_ROOT_PASSWORD, "/path/backup/file"),
                          connect_to_server(TEMP_SERVER_NAME, MY_USERNAME, MY_PASS))
     return 'MONGO backup of all dbs is done!!'
 
-
+# TODO: Change to valid command with format!
 # TODO: Change the server name, the username and the password!!! + get backup file as parameter!!!
 def mongo_restore_all():
-    output = run_command(MONGO_RESTORE_ALL_COMMAND + "/path/to/backup/file_name",
+    output = run_command(MONGO_RESTORE_ALL_COMMAND.format(MONGO_ROOT_USER, MONGO_ROOT_PASSWORD, "/path/backup/file"),
                          connect_to_server(TEMP_SERVER_NAME, MY_USERNAME, MY_PASS))
     return 'MONGO restore of all dbs is done!!'
 
@@ -49,4 +51,39 @@ def get_mongo_backup_files_by_type(resourceType):
 
 
 def create_mongo_db(dbName):
-    pass
+    try:
+        client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+        newDB = client[dbName]
+        sampleCollection = newDB["Delete_Me_Collection"]
+        result = sampleCollection.insert_one({"note": "Delete me"})
+        dbsList = client.list_database_names()
+        return "%s db Created, Delete the sample collection and edit as you want" % dbName
+    except pymongo.errors.ConnectionFailure as e:
+        error = "Could not connect to server: %s" % e
+        return error
+
+
+# TODO: Check if working!!!!!!!!
+def create_mongo_user(dbName, username, password):
+    try:
+        client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+        db = client[dbName]
+        db.command("createUser", username, pwd=password, roles=["dbOwner"])
+        return "%s db Created, Delete the sample collection and edit as you want" % username
+    except pymongo.errors.ConnectionFailure as e:
+        error = "Could not connect to server: %s" % e
+        return error
+    '''
+    To create a user:
+    db.command("createUser", "admin", pwd="password", roles=["root"])
+    
+    To create a read-only user:
+    db.command("createUser", "user", pwd="password", roles=["read"])
+    
+    To change a password:
+    db.command("updateUser", "user", pwd="newpassword")
+    
+    Or change roles:
+    db.command("updateUser", "user", roles=["readWrite"])
+    db.command("updateUser", "user", roles=["readWrite"])
+    '''
